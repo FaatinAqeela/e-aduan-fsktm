4d8e06b79312c0e6ab41c6ed52a833eb96f046a5
Faatin Aqeela <faatinaqeela@gmail.com>
2020-02-03 15:13:19 +0800

    tambah card

--- lib/sejarahaduan.dart
+++ lib/sejarahaduan.dart
@@ -7,55 +7,152 @@ class SejarahAduan extends StatefulWidget {
 
 class _SejarahAduanState extends State<SejarahAduan>
     with SingleTickerProviderStateMixin {
-  TabController controller;
+  TabController _tabController;
+  ScrollController _scrollViewController;
 
   @override
   void initState() {
-    controller = new TabController(vsync: this, length: 3);
     super.initState();
+    _tabController = TabController(vsync: this, length: 3);
+    _scrollViewController = ScrollController(initialScrollOffset: 0.0);
   }
 
-  /*@override
+  @override
   void dispose() {
-    controller.dispose;
+    _tabController.dispose();
+    _scrollViewController.dispose();
     super.dispose();
-  }*/
+  }
 
   @override
   Widget build(BuildContext context) {
+    // return DefaultTabController(
+    //   length: 2,
+    //       child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Example App'),
+    //       bottom: TabBar(
+    //         tabs: <Widget>[
+    //           Tab(
+    //             text: "Home",
+    //             icon: Icon(Icons.home),
+    //           ),
+    //           Tab(
+    //             text: "Example page",
+    //             icon: Icon(Icons.help),
+    //           )
+    //         ],
+    //       ),
+    //     ),
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sejarah Aduan"),
-        bottom: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-                icon: new Text(
-              "Disemak",
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Montserrat'),
-            )),
-            new Tab(
-                icon: new Text(
-              "Selesai",
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Montserrat'),
-            )),
-            new Tab(
-                icon: new Text(
-              "Tidak Selesai",
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Montserrat'),
-            )),
+      body: NestedScrollView(
+        controller: _scrollViewController,
+        headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text('Sejarah Aduan'),
+              pinned: true,
+              floating: true,
+              forceElevated: boxIsScrolled,
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    text: "Disemak",
+                    icon: Icon(Icons.query_builder),
+                  ),
+                  Tab(
+                    text: "Selesai",
+                    icon: Icon(Icons.done),
+                  ),
+                  Tab(
+                    text: "Tidak Selesai",
+                    icon: Icon(Icons.help),
+                  )
+                ],
+                controller: _tabController,
+              ),
+            )
+          ];
+        },
+        body: TabBarView(
+          children: <Widget>[
+            Disemak(),
+            Selesai(),
+            TidakSelesai(),
+          ],
+          controller: _tabController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.control_point),
+        onPressed: () {
+          _tabController.animateTo(1,
+              curve: Curves.bounceInOut, duration: Duration(milliseconds: 10));
+
+          // _scrollViewController.animateTo(
+          //     _scrollViewController.position.minScrollExtent,
+          //     duration: Duration(milliseconds: 1000),
+          //     curve: Curves.decelerate);
+
+          _scrollViewController
+              .jumpTo(_scrollViewController.position.maxScrollExtent);
+        },
+      ),
+    );
+  }
+}
+
+class Disemak extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Selesai extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TidakSelesai extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
           ],
         ),
       ),
