a2a9303b67b48bfa9b5bf8cd35e218c6285aab8e
Faatin Aqeela <faatinaqeela@gmail.com>
2020-02-12 02:39:09 +0800

    design profil ui

--- lib/profil.dart
+++ lib/profil.dart
@@ -1,96 +1,248 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class Profil extends StatefulWidget {
-  final namapenuh;
-  Profil(this.namapenuh);
-
   @override
-  _ProfilState createState() => _ProfilState();
+  MapScreenState createState() => MapScreenState();
 }
 
-class _ProfilState extends State<Profil> {
+class MapScreenState extends State<Profil> with SingleTickerProviderStateMixin {
+  bool _status = false;
+  final FocusNode myFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text('Profil'),
+        title: Text("Profil"),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 4,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                gradient: new LinearGradient(
-                  colors: [
-                    const Color(0xff90A4AE),
-                    const Color(0xFFFAFAFA),
-                  ],
-                  begin: Alignment.centerRight,
-                  end: new Alignment(-1.0, -1.0),
+      body: new Container(
+        color: Colors.white,
+        child: new ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                new Container(
+                  height: 190.0,
+                  color: Colors.blueGrey[300],
+                  child: new Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child:
+                            new Stack(fit: StackFit.loose, children: <Widget>[
+                          new Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new Container(
+                                  width: 140.0,
+                                  height: 140.0,
+                                  decoration: new BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: new DecorationImage(
+                                      image: NetworkImage(
+                                          "https://thenypost.files.wordpress.com/2019/04/pet-sematary-cat-2a.jpg?quality=90&strip=all&w=1054&h=702&crop=1"),
+
+                                      // new ExactAssetImage(
+                                      //     'images/avatar-profile.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        ]),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              child: Stack(
-                children: <Widget>[
-                  profilePage("${widget.namapenuh}", "Flutter Developer",
-                      "Paris", "200", "www.baileyromero.com"),
-                  ProfileImageWidget(),
-                ],
-              ),
+                new Container(
+                  color: Color(0xffFFFFFF),
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 10.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                            padding: EdgeInsets.only(
+                                left: 25.0, right: 25.0, top: 10.0),
+                            child: new Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                new Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    new Text(
+                                      'Nama',
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )),
+                        Padding(
+                            padding: EdgeInsets.only(
+                                left: 25.0, right: 25.0, top: 0.0),
+                            child: new Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                new Flexible(
+                                  child: new TextField(
+                                    decoration: const InputDecoration(
+                                        hintText: "Faatin Aqeela Bt Ghazali"),
+                                    enabled: _status,
+                                  ),
+                                ),
+                              ],
+                            )),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 25.0),
+                          child: new Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Expanded(
+                                child: Container(
+                                  child: new Text(
+                                    'No. Matrik',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                flex: 2,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  child: new Text(
+                                    'Fakulti/Jabatan',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                flex: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 0.0),
+                          child: new Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Flexible(
+                                child: Padding(
+                                  padding: EdgeInsets.only(right: 10.0),
+                                  child: new TextField(
+                                    decoration: const InputDecoration(
+                                        hintText: "ci170022"),
+                                    enabled: _status,
+                                  ),
+                                ),
+                                flex: 2,
+                              ),
+                              Flexible(
+                                child: new TextField(
+                                  decoration:
+                                      const InputDecoration(hintText: "FSKTM"),
+                                  enabled: _status,
+                                ),
+                                flex: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 25.0),
+                          child: new Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Expanded(
+                                child: Container(
+                                  child: new Text(
+                                    'No. Telefon',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                flex: 2,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  child: new Text(
+                                    'Kategori Pengguna',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                flex: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 2.0),
+                          child: new Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Flexible(
+                                child: Padding(
+                                  padding: EdgeInsets.only(right: 10.0),
+                                  child: new TextField(
+                                    decoration: const InputDecoration(
+                                        hintText: "01122334455"),
+                                    enabled: _status,
+                                  ),
+                                ),
+                                flex: 2,
+                              ),
+                              Flexible(
+                                child: new TextField(
+                                  decoration: const InputDecoration(
+                                      hintText: "Pelajar"),
+                                  enabled: _status,
+                                ),
+                                flex: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  //Center Widget
-  Center profilePage(String profileName, String jobDescription, String location,
-      String followerVal, String webLink) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          width: 300.0,
-          height: 300.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.0),
-            color: Color(0xffffffff),
-          ),
+          ],
         ),
       ),
     );
   }
-}
-
-class ProfileImageWidget extends StatefulWidget {
-  @override
-  _ProfileImageWidgetState createState() => _ProfileImageWidgetState();
-}
-
-class _ProfileImageWidgetState extends State<ProfileImageWidget> {
-  var height = 160.0;
-  var width = 160.0;
 
   @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      top: 10.0,
-      left: 100.0,
-      child: Container(
-        width: width,
-        height: height,
-        decoration: new BoxDecoration(
-          shape: BoxShape.circle,
-          image: new DecorationImage(
-            fit: BoxFit.fill,
-            image: new NetworkImage(
-                "https://thenypost.files.wordpress.com/2019/04/pet-sematary-cat-2a.jpg?quality=90&strip=all&w=1054&h=702&crop=1"),
-          ),
-        ),
-      ),
-    );
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    myFocusNode.dispose();
+    super.dispose();
   }
 }
